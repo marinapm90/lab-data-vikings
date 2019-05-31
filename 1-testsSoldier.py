e3e7ff63aa357105ad32200de015a2c2c0787a0f
Javier Luna molina <javierlunamolina@gmail.com>
2019-06-01 00:25:48 +0200

    Refactor code to make it look pythonic. Add description to failing tests

--- 1-testsSoldier.py
+++ 1-testsSoldier.py
@@ -5,43 +5,60 @@ from inspect import signature
 
 class TestSoldier(unittest.TestCase):
 
-    @classmethod
-    def setUp(cls):
-        cls.strength = 150
-        cls.health = 300
-        cls.soldier = Soldier(cls.health, cls.strength)
+    def setUp(self):
+        self.strength = 150
+        self.health = 300
+        self.soldier = Soldier(self.health, self.strength)
 
-    def testConstructorSignature(self):
-        self.assertEqual(len(signature(Soldier).parameters), 2)
+    def test_init_has_two_params(self):
+        self.assertEqual(len(signature(Soldier).parameters), 2,
+                         "A Soldier's initializer (__init__ method) must accept two parameters!")
 
-    def testHealth(self):
-        self.assertEqual(self.soldier.health, self.health)
+    def test_health_attr_exists(self):
+        self.assertTrue(hasattr(self.soldier, 'health'), "Soldier class doesn't have a 'health' attribute!")
 
-    def testStrenght(self):
-        self.assertEqual(self.soldier.strength, self.strength)
+    def test_health_works_ok(self):
+        self.assertEqual(self.soldier.health, self.health, "Something is up with the Soldier's 'health' attribute")
 
-    def testAttackShouldBeFunction(self):
-        self.assertEqual(callable(self.soldier.attack), True)
+    def test_strenght_attr_exists(self):
+        self.assertTrue(hasattr(self.soldier, 'strength'), "Soldier class doesn't have a 'strength' attribute")
 
-    def testAttackHasNoParams(self):
-        self.assertEqual(len(signature(self.soldier.attack).parameters), 0)
+    def test_strenght_works_ok(self):
+        self.assertEqual(self.soldier.health, self.health, "Something is up with the Soldier's 'strength' attribute")
 
-    def testAttackRetunsStrength(self):
-        self.assertEqual(self.soldier.attack(), self.strength)
+    def test_soldier_has_attack_method(self):
+        self.assertTrue(hasattr(self.soldier, 'attack'), "Soldier class must have an 'attack' method!")
 
-    def testReceivesDamage(self):
-        self.assertEqual(callable(self.soldier.receiveDamage), True)
+    def test_soldier_attack_is_method(self):
+        self.assertTrue(callable(self.soldier.attack), "Soldier class 'attack' attribute must be a method!")
 
-    def testReceivesDamageHasParams(self):
-        self.assertEqual(
-            len(signature(self.soldier.receiveDamage).parameters), 1)
+    def test_soldier_attack_method_no_args(self):
+        self.assertEqual(len(signature(self.soldier.attack).parameters), 0,
+                         "Soldier's method 'attack' must not accept any argument at all")
 
-    def testReceiveDamageReturnNone(self):
-        self.assertEqual(self.soldier.receiveDamage(50), None)
+    def test_soldier_attack_returns_soldier_strenght(self):
+        self.assertEqual(self.soldier.attack(), self.strength,
+                         "Soldier's method 'attack' must return the soldier's strenght")
 
-    def testCanReceiveDamage(self):
-        self.soldier.receiveDamage(50)
-        self.assertEqual(self.soldier.health, self.health - 50)
+    def test_soldier_has_receive_damage_method(self):
+        self.assertTrue(hasattr(self.soldier, 'receive_damage'), "Soldier must have a 'receive_damage' method")
+
+    def test_soldier_receive_damage_is_method(self):
+        self.assertTrue(callable(self.soldier.receive_damage),
+                        "Soldier class 'receive_damage' attribute must be a method!")
+
+    def test_soldier_receive_damage_accepts_one_parameter(self):
+        self.assertEqual(len(signature(self.soldier.receive_damage).parameters), 1,
+                         "Soldier's 'receive_damage' method must accept one argument")
+
+    def test_receive_damage_returns_none(self):
+        self.assertEqual(self.soldier.receive_damage(50), None,
+                         "Soldier's 'receive_damage' method must return nothing!")
+
+    def test_soldier_can_receive_damage(self):
+        dmg = 50
+        self.soldier.receive_damage(dmg)
+        self.assertEqual(self.soldier.health, self.health - dmg, "Soldier 'receive_damage' doesn't work as expected")
 
 
 if __name__ == '__main__':
